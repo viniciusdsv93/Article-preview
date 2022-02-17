0e5cf83935139a7a574c052a3dd19204243c9e81
viniciusdsv93 <viniciusdsv93@gmail.com>
2022-02-17 07:08:34 -0300

    Updated active states in the mobile version

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const shareButton = document.querySelector('.share-button');
+const bodyItem = document.querySelector('body');
 
 shareButton.addEventListener('click', event => {
     document.querySelector('.share-area').classList.toggle('hidden');
+
+    document.querySelector('.share-container--mobile').classList.toggle('hidden-mobile');
+
+    document.querySelector('.container-text--profile').classList.toggle('hidden-mobile');  
     
+})
+
+const shareButtonMobile = document.querySelector('.profile-share--mobile');
+
+shareButtonMobile.addEventListener('click', event => {
+    
+    document.querySelector('.share-container--mobile').classList.toggle('hidden-mobile');
+
+    document.querySelector('.container-text--profile').classList.toggle('hidden-mobile');  
 })
